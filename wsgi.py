0bf1e2ad055e0a6cc6253a56f3b5814ac4ee6451
Taylor <taylor@taylor-S400CA.(none)>
2015-11-24 21:08:16 -0600

    EDIT:production settings

--- wsgi.py
+++ wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.prod")
 
 
 from django.core.wsgi import get_wsgi_application
