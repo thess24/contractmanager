c8b75920f485f901d35b4e7fb68e35523970c608
Taylor <taylor@taylor-S400CA.(none)>
2015-12-28 19:17:21 -0600

    db changes for ebs

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -47,7 +47,18 @@ DEBUG = True
 
 
 ########## DATABASE CONFIGURATION
-DATABASES = postgresify()
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': os.environ['RDS_DB_NAME'],
+		'USER': os.environ['RDS_USERNAME'],
+		'PASSWORD': os.environ['RDS_PASSWORD'],
+		'HOST': os.environ['RDS_HOSTNAME'],
+		'PORT': os.environ['RDS_PORT'],
+	}
+}
+    
 ########## END DATABASE CONFIGURATION
 
 
