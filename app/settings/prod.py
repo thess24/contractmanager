99b0b0c367d9fe509630d9d912df3d942e8ca65f
Taylor <taylor@taylor-S400CA.(none)>
2015-12-29 19:14:24 -0600

    staticfiles

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -84,14 +84,14 @@ DEFAULT_FILE_STORAGE = 'app.settings.s3utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'app.settings.s3utils.StaticRootS3BotoStorage'
 
 # # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-# AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
+AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
 
 # # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
 AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY', '')
 AWS_STORAGE_BUCKET_NAME = environ.get('AWS_STORAGE_BUCKET_NAME', '')
-# AWS_AUTO_CREATE_BUCKET = True
-# AWS_QUERYSTRING_AUTH = False
+AWS_AUTO_CREATE_BUCKET = True
+AWS_QUERYSTRING_AUTH = False
 
 # # AWS cache settings, don't change unless you know what you're doing:
 AWS_EXPIRY = 60 * 60 * 24 * 7
