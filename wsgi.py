366b53ebf95326942e6fa9893354470dd85421e3
Taylor <taylor@taylor-S400CA.(none)>
2015-12-29 18:23:45 -0600

    changeback and fix collectstatic

--- wsgi.py
+++ wsgi.py
@@ -19,8 +19,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.dev")
 
 
 from django.core.wsgi import get_wsgi_application
-# from dj_static import Cling
+from dj_static import Cling
 
-# application = Cling(get_wsgi_application())
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
 
