54ed46d93f155d78f656a5720118685b58ce4a1b
Taylor <taylor@taylor-S400CA.(none)>
2015-12-29 18:10:46 -0600

    trying to get s3 online

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -68,68 +68,42 @@ DATABASES = {
 ########## END CACHE CONFIGURATION
 
 
-########## CELERY CONFIGURATION
-# See: http://docs.celeryproject.org/en/latest/configuration.html#broker-transport
-# BROKER_TRANSPORT = 'amqplib'
-
-# # Set this number to the amount of allowed concurrent connections on your AMQP
-# # provider, divided by the amount of active workers you have.
-# #
-# # For example, if you have the 'Little Lemur' CloudAMQP plan (their free tier),
-# # they allow 3 concurrent connections. So if you run a single worker, you'd
-# # want this number to be 3. If you had 3 workers running, you'd lower this
-# # number to 1, since 3 workers each maintaining one open connection = 3
-# # connections total.
-# #
-# # See: http://docs.celeryproject.org/en/latest/configuration.html#broker-pool-limit
-# BROKER_POOL_LIMIT = 3
-
-# # See: http://docs.celeryproject.org/en/latest/configuration.html#broker-connection-max-retries
-# BROKER_CONNECTION_MAX_RETRIES = 0
-
-# # See: http://docs.celeryproject.org/en/latest/configuration.html#broker-url
-# BROKER_URL = environ.get('RABBITMQ_URL') or environ.get('CLOUDAMQP_URL')
-
-# # See: http://docs.celeryproject.org/en/latest/configuration.html#celery-result-backend
-# CELERY_RESULT_BACKEND = 'amqp'
-########## END CELERY CONFIGURATION
-
-
 ########## STORAGE CONFIGURATION
 # See: http://django-storages.readthedocs.org/en/latest/index.html
 INSTALLED_APPS += (
-    # 'storages',
+    'storages'
     # 'djrill'
 )
 
 # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-# STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 # had to do this to get a3 to serve static and media
-# DEFAULT_FILE_STORAGE = 'homepage.settings.s3utils.MediaRootS3BotoStorage'
-# STATICFILES_STORAGE = 'homepage.settings.s3utils.StaticRootS3BotoStorage'
+DEFAULT_FILE_STORAGE = 'homepage.settings.s3utils.MediaRootS3BotoStorage'
+STATICFILES_STORAGE = 'homepage.settings.s3utils.StaticRootS3BotoStorage'
 
 # # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
 # AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
 
 # # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-# AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID', '')
-# AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY', '')
-# AWS_STORAGE_BUCKET_NAME = environ.get('AWS_STORAGE_BUCKET_NAME', '')
+AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY', '')
+AWS_STORAGE_BUCKET_NAME = environ.get('AWS_STORAGE_BUCKET_NAME', '')
 # AWS_AUTO_CREATE_BUCKET = True
 # AWS_QUERYSTRING_AUTH = False
 
 # # AWS cache settings, don't change unless you know what you're doing:
-# AWS_EXPIRY = 60 * 60 * 24 * 7
-# AWS_HEADERS = {
-#     'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIRY,
-#         AWS_EXPIRY)
-# }
+AWS_EXPIRY = 60 * 60 * 24 * 7
+AWS_HEADERS = {
+    'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIRY,
+        AWS_EXPIRY)
+}
 
 # # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-# S3_URL = 'https://s3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
-# STATIC_URL = S3_URL + '/static/'
-# MEDIA_URL = S3_URL + '/media/'
+S3_URL = 'https://s3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = S3_URL + '/static/'
+MEDIA_URL = S3_URL + '/media/'
 
 # COMPRESS_URL = STATIC_URL 
 ########## END STORAGE CONFIGURATION
@@ -143,7 +117,7 @@ SECRET_KEY = environ.get('SECRET_KEY', SECRET_KEY)
 
 ########## ALLOWED HOSTS CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['.herokuapp.com', '.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '.elasticbeanstalk.com', '.baldurhealthcare.com']
 ########## END ALLOWED HOST CONFIGURATION
 
 
